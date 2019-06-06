b25da9e11291f2a24a61da2d64436d2e7824a5bd
mxavier91 <mxavier17@gmail.com>
2019-06-06 09:26:00 -0400

    Started my create-react-app. Created a Navbar, and worked on CSS file.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Navbar from './Components/layout/Navbar'
+import './App.css';
+
+// With Class based Components, you MUST remember to use the keyword extends
+class App extends Component {
+
+  // render() is a LifeCycle Method. There are other lifecycle methods, but render() is the only one that is REQUIRED!!!!!
+  render() {
+    // JSX MUST HAVE one parent div element
+    return (
+      // Use React.Fragment, is you don't want the div element to show up in the DOM
+      // <Fragment>
+      <div className="App">
+        <Navbar icon="fab fa-github" title="Github Finder" />
+      </div>
+      // </Fragment>
+    );
+  }
+}
+
+export default App;
