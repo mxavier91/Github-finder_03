24d2a8d0c2e9973be26bd3199b4e4cb76f9c05f9
mxavier91 <mxavier17@gmail.com>
2019-06-07 14:30:49 -0400

    Add a Clear button and refactored some of the code. I created a Clear button that only displays once a User has searched from a github user. I also refactored some of my code by using destructoring.

--- src/Components/Users/Search.js
+++ src/Components/Users/Search.js
@@ -8,7 +8,10 @@ class Search extends Component {
   }
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+
   }
 
   onSubmit = (e) => {
@@ -20,12 +23,16 @@ class Search extends Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value })
 
   render() {
+    const { showClear, clearUsers } = this.props
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
           <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
           <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+        )}
       </div>
     )
   }
