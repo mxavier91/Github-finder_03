b8a29a14d8e8987a4c488f0aa916833de514b8bf
mxavier91 <mxavier17@gmail.com>
2019-06-11 14:14:37 -0400

    Added Repos to a Github User's profile. On a Github User's profile they will now see the 5 most recent repos on that profile.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -53,6 +54,19 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get Users Repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
   // Clear Users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -63,7 +77,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading, alert } = this.state;
+    const { users, user, repos, loading, alert } = this.state;
     return (
       <Router>
         <div className="App">
@@ -95,7 +109,9 @@ class App extends Component {
                     <User
                       {...props}
                       getUser={this.getUser}
+                      getUserRepos={this.getUserRepos}
                       user={user}
+                      repos={repos}
                       loading={loading}
                     />
                   </Fragment>
